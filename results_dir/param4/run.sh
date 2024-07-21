abbb1f1e8867a7b8b09c30dc17264c119c5e83f7
Ashton S Tang <ashtont@nature.cs.uchicago.edu>
2024-07-20 20:27:19 -0500

    Add results_dir from GEAR testing.

---
+++ results_dir/param4/run.sh
@@ -0,0 +1,1 @@
+python gear-generate-responses.py --model lmsys/longchat-7b-16k --rank 4 --rankv 4 --left 0.1 --quantize_bit 8 --group_size 8 --loop 3 --top_k 2 --compress_method GEAR --streaming --streaming_gap 64 --start_file 0 --end_file 50 --file_dir /dataheart/ashton/CacheGen2/CacheGen/test_data/16_prompts --results_dir results_dir/param4
