5c6049c84252bf0a8bba36445882400e0de0c551
Ashton S Tang <ashtont@nature.cs.uchicago.edu>
2024-07-20 21:30:06 -0500

    Remove results-dir.

--- results_dir/param1/run.sh
+++
@@ -1,1 +0,0 @@
-python gear-generate-responses.py --model lmsys/longchat-7b-16k --rank 1 --rankv 1 --left 0.0001 --quantize_bit 2 --group_size 8 --loop 3 --top_k 2 --compress_method GEAR --streaming --streaming_gap 64 --start_file 0 --end_file 50 --file_dir /dataheart/ashton/CacheGen2/CacheGen/test_data/16_prompts --results_dir results_dir --kvcache_dir kvcache_dir
